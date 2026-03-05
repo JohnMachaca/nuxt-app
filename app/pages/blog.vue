1ad64dcd9c15421c8fb973de1a08cad889fe2930
unknown <jwmachaca@gmail.com>
2026-03-05 13:35:26 -0500

    rutas

---
+++ app/pages/blog.vue
@@ -0,0 +1,4 @@
+<script></script>
+<template>
+    <h1>Hola soy blog</h1>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Hola mundo soy el index de pages</h1>
+    <ul>
+        <li><NuxtLink to="/ejercicios/form-watch">form-watch</NuxtLink></li>
+        <li><NuxtLink to="/ejercicios/holamundo">Holamundo</NuxtLink></li>
+    </ul>
+</template>
