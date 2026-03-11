b8492a95662ce092f59632ef80a83ec84da23beb
John Machaca <jwmachaca@gmail.com>
2026-03-11 18:36:28 -0500

    p.trabajo

---
+++ app/pages/ejercicios/adduser.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue"
+const usuarios = ref([])
+const nombre = ref("")
+
+const agregar = ()=>{
+  usuarios.value.push(nombre.value)
+  nombre.value=""
+}
+
+</script>
+<template>
+<h1>Agregar usuarios</h1>
+<input v-model="nombre"></input>
+<button @click="agregar">Agregar</button>
+<ul>
+    <li v-for="usuario in usuarios" :key="usuario">{{usuario}}</li>
+</ul>
+</template>
