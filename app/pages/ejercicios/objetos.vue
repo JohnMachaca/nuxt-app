5e46f7e824329ffc7a4e0937f6bdb7917b734c77
John Machaca <jwmachaca@gmail.com>
2026-03-17 21:07:13 -0500

    objetos

---
+++ app/pages/ejercicios/objetos.vue
@@ -0,0 +1,13 @@
+<script setup>
+const objeto = {}
+objeto["manzana"]=2
+objeto["pera"] =1
+objeto["banana"] =4
+objeto.papaya = 10
+const fruta = "melon"
+objeto[fruta]   =2
+const fruta2 = "tomate"
+objeto.fruta2=8
+console.log(objeto)
+</script>
+<template></template>
---
+++ app/pages/ejercicios/reducesumanumeros.vue
@@ -0,0 +1,7 @@
+<script setup>
+const numeros = [1, 2, 3, 4,4]
+
+const suma = numeros.reduce((ac, num)=>{return ac*num},1)
+</script>
+<template>{{ suma }}</template>
+
